996d3e22816a44873bc7d219caa9b25a57ae7f9b
Mathis Lefranc <lefrancmathis@gmail.com>
2023-02-23 10:58:41 -0500

    new commit

--- index.js
+++ index.js
@@ -44,11 +44,14 @@ function generateEvent() {
   // Get name of the event
   var name = document.getElementById("name").value;
   // Get schedule type of the event
+  var scheduleType;
   var schedule;
   if (document.getElementById('scheduleType').value == "days-of-the-week") {
     schedule = getCheckboxValues();
+    scheduleType = "days-of-the-week";
   } else if (document.getElementById('scheduleType').value == "specific-date") {
     schedule = document.getElementById("specificDate").value;
+    scheduleType = "specific-date";
   }
   // Get lower bound of event hour range
   var hourLow = document.getElementById('hourLow').value;
@@ -64,15 +67,15 @@ function generateEvent() {
   const event = {
     id: null,
     name: name,
+    scheduleType: scheduleType,
     schedule: schedule,
     hourLow: hourLow,
     hourHigh: hourHigh,
     duration: duration,
     minParticipant: minParticipant,
-    maxParticipant: maxParticipant
+    maxParticipant: maxParticipant,
+    participants: []
   }
-
-
 
   if (localStorage.getItem("events") != null) {
     var events = localStorage.getItem("events");
@@ -93,18 +96,6 @@ function generateEvent() {
 }
 
 
-function createParticipant() {
-  // Get name of the participant
-  var firstName = document.getElementById("firstName").value;
-  var lastName = document.getElementById("lastName").value;
-  var password = document.getElementById("password").value;
-
-  var days = document.getElementsByClassName("day");
- 
-  console.log(days);
-
-}
-
 function displaySchedule() {
   var scheduleType = document.getElementById("scheduleType").value;
   if (scheduleType == "days-of-the-week") {
@@ -123,7 +114,7 @@ function displaySchedule() {
 function visitEvent() {
   var eventID = document.getElementById("eventIdEventPage").value;
 
-  if (getEvent(eventID) != -1) {
+  if (getEvent() != -1) {
     document.getElementById("eventIdEventPage").value = "";
     localStorage.setItem("currentEventID", JSON.stringify(eventID));
     window.location.href = "dashboard.html";
@@ -137,7 +128,7 @@ function populateDashboard() {
   document.getElementById("id-to-copy").innerHTML = eventID;
   document.getElementById("url-to-copy").innerHTML = "www.avail2ball.com/" + eventID;
 
-  var event = getEvent(eventID);
+  var event = getEvent();
 
   var days = event.schedule;
   if (typeof days == "string") {
@@ -151,7 +142,10 @@ function populateDashboard() {
   }
 }
 
-function getEvent(id) {
+function getEvent() {
+
+  var id = JSON.parse(localStorage.getItem("currentEventID"));
+
   var events = JSON.parse(localStorage.getItem("events"));
   if (events != null) {
     for (i = 0; i < events.length; i++) {
@@ -166,44 +160,144 @@ function getEvent(id) {
 
 function getNextDay() {
 
-  var event  = getEvent(JSON.parse(localStorage.getItem("currentEventID")));
+  var event  = getEvent();
 
   var days = event.schedule;
+
   if (days.length > 1) {
-    for (i = 0; i < days.length; i++) {
-      if (days[i] == document.getElementById("dashboardDate").innerHTML) {
-        if (i < days.length - 1) {
-          document.getElementById("dashboardDate").innerHTML = days[i + 1];
-        } else {
-          document.getElementById("dashboardDate").innerHTML = days[0];
-        }
-        break;
-      }
+    if (days.indexOf(document.getElementById("dashboardDate").innerHTML) < days.length - 1) {
+      document.getElementById("dashboardDate").innerHTML = days[days.indexOf(document.getElementById("dashboardDate").innerHTML) + 1];
+    } else {
+      document.getElementById("dashboardDate").innerHTML = days[0];
     }
   }
+  
 } 
 
 function getPreviousDay() {
-  var event  = getEvent(JSON.parse(localStorage.getItem("currentEventID")));
+  var event  = getEvent();
 
   var days = event.schedule;
+
   if (days.length > 1) {
-    for (i = 0; i < days.length; i++) {
-      if (days[i] == document.getElementById("dashboardDate").innerHTML && i > 0) {
-        document.getElementById("dashboardDate").innerHTML = days[i - 1];
-      } else {
-        document.getElementById("dashboardDate").innerHTML = days[days.length - 1];
-      }
+    if (days.indexOf(document.getElementById("dashboardDate").innerHTML) > 0) {
+      document.getElementById("dashboardDate").innerHTML = days[days.indexOf(document.getElementById("dashboardDate").innerHTML) - 1];
+    } else {
+      document.getElementById("dashboardDate").innerHTML = days[days.length - 1];
     }
+  }
+}
+
+
+function populateMemberRegistration() {
+
+  var event = getEvent();
+
+  // If schedule type is single day  
+  if (event.scheduleType == "specific-date") 
+  {
+    document.getElementById("dailyAvailabilitiesContainer").innerHTML += '<div class="day" id="day"><div class="availabilitySlotContainer"><div id="availabilitySlot" class="availability-time"><input type="time" id="from" name="after" min="09:00" max="00:00" required=""><p>to</p><input type="time" id="to" name="after" min="00:00" max="00:00" required=""><img onclick="deleteSlot(this)" src="images/trash-black.png" alt="bin-icon"></div></div><div onclick="addSlot(this)" class="add-a-slot"><img src="images/+iconv2.png" alt="+icon"><p>Add a slot</p></div></div>';
+  }
+  // Else
+  else if (event.scheduleType == "days-of-the-week")
+  {
+    var defaultDayNumber = 0;
+    localStorage.setItem("lastDayNumber", JSON.stringify(defaultDayNumber));
+    
+    var days  = getEvent().schedule;
+    var dayOptions = "";
+    for (var i = 0; i < days.length; i++) {
+      dayOptions += '<option value="select">' + days[i] + '</option>';
+    }
+
+    document.getElementById("dailyAvailabilitiesContainer").insertAdjacentHTML("beforeend", '<div class="day" id="day' + defaultDayNumber + '"><div class="availability-day"><select class="" name=""><option value="select">Select the day</option>' + dayOptions + '</select><img onclick="deleteDay(this)" src="images/trash-red.png" alt="red-bin-icon"></div><div class="availabilitySlotContainer"><div id="availabilitySlot" class="availability-time"><input type="time" id="from" name="after" min="09:00" max="00:00" required=""><p>to</p><input type="time" id="to" name="after" min="00:00" max="00:00" required=""><img onclick="deleteSlot(this)" src="images/trash-black.png" alt="bin-icon"></div></div><div onclick="addSlot(this)" class="add-a-slot"><img src="images/+iconv2.png" alt="+icon"><p>Add a slot</p></div></div>');
+
+    document.getElementById("dailyAvailabilitiesContainer").insertAdjacentHTML("afterend", '<div class="add-a-day"><img onclick="addDay()" src="images/+iconv2.png" alt="+icon"><p>Add a day</p></div>');
   }
 }
 
 function addDay() {
-  var content = document.getElementById("dailyAvailabilitiesContainer").innerHTML;
-  content += '<div class="day"><div class="availability-day"><select class="" name=""><option value="select">Select the day</option><option value="monday">Monday</option><option value="wednesday">Wednesday</option><option value="saturday">Saturday</option><option value="sunday">Sunday</option></select><img onclick="deleteDay()" src="images/trash-red.png" alt="red-bin-icon"></div><div class="availability-time"><input type="time" id="from" name="after" min="09:00" max="00:00" required=""><p>to</p><input type="time" id="to" name="after" min="00:00" max="00:00" required=""><img src="images/trash-black.png" alt="bin-icon"></div><div class="add-a-slot"><img src="images/+iconv2.png" alt="+icon"><p>Add a slot</p></div></div>'
-  document.getElementById("dailyAvailabilitiesContainer").innerHTML = content;
+  document.getElementById("dailyAvailabilitiesContainer").insertAdjacentHTML("beforeend", '<div class="day" id="day"><div class="availability-day"><select class="" name=""><option value="select">Select the day</option></select><img onclick="deleteDay(this)" src="images/trash-red.png" alt="red-bin-icon"></div><div class="availabilitySlotContainer"><div id="availabilitySlot" class="availability-time"><input type="time" id="from" name="after" min="09:00" max="00:00" required=""><p>to</p><input type="time" id="to" name="after" min="00:00" max="00:00" required=""><img onclick="deleteSlot(this)" src="images/trash-black.png" alt="bin-icon"></div></div><div onclick="addSlot(this)" class="add-a-slot"><img src="images/+iconv2.png" alt="+icon"><p>Add a slot</p></div></div>');
 }
 
-function deleteDay() {
+function deleteDay(e) {
+  e.parentNode.parentNode.remove();
+}
+
+function addSlot(e) {
+  e.previousSibling.insertAdjacentHTML("afterend", '<div id="availabilitySlot" class="availability-time"><input type="time" id="from" name="after" min="09:00" max="00:00" required=""><p>to</p><input type="time" id="to" name="after" min="00:00" max="00:00" required=""><img onclick="deleteSlot(this)" src="images/trash-black.png" alt="bin-icon"></div>');
+}
+
+function deleteSlot(e) {
+  e.parentNode.remove();
+}
+
+function createParticipant() {
+  // Get name of the participant
+  var firstName = document.getElementById("fname").value;
+  var lastName = document.getElementById("lname").value;
+  var password = document.getElementById("pword").value;
+
+  const participant = {
+    participantId: null,
+    firstName: firstName,
+    lastName: lastName,
+    password: password,
+    scheduleType: null,
+    availabilities: null
+  }
+
+  var event = getEvent();
+
+  if (event.participants.length == 0) {
+    participant.participantId = 0;
+  } else {
+    participant.participantId = event.participants[event.participants.length - 1].participantId + 1;
+  }
+
+  if (event.scheduleType == "specific-date") 
+  {
+    participant.scheduleType = "specific-date";
+
+    var availabilities = [];
+
+    var slots = document.getElementsByClassName("availability-time");
+    for (var i = 0; i < slots.length; i++) {
+      var availability = []
+      availability.push(slots[i].children[0].value);
+      availability.push(slots[i].children[2].value);
+      availabilities.push(availability);
+    }
+    participant.availabilities = availabilities;
+  }
+  // Else
+  else if (event.scheduleType == "days-of-the-week")
+  {
+    participant.scheduleType = "days-of-the-week";
+
+    var availabilities = {};
+
+
+    participant.availabilities = availabilities;
+
+  }
+
+
+  // Add participants to event
+  var events = JSON.parse(localStorage.getItem("events"));
+  var event = getEvent();
+  event.participants.push(participant);
+
+  // Add updated event to events list
+  for (var i = 0; i < events.length; i++) {
+    if (events[i].eventID == event.eventID) {
+      events[i] = event;
+    }
+  }
   
+  // Update local storage variable with updated events list
+  localStorage.setItem("events", JSON.stringify(events));
+
+  // Navigate to dashboard
+  window.location.href = "dashboard.html";
 }
